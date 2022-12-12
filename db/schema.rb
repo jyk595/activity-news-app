3bdc56f053ade7065b6fba8d5d8d2e71da368c68
John Kim <jyk595@gmail.com>
2022-12-12 12:28:13 -0800

    new postgresql addon

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_09_29_205603) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "plpgsql" 
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
